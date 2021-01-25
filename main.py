c87bce428ad9d5f00bb8330cee542163fe25917d
oceaster <oceaster@gmail.com>
2021-01-25 23:51:54 +0000

    added install command.

---
+++ main.py
@@ -0,0 +1,29 @@
+from os import system
+from sys import argv, executable
+
+
+def run(command, argIndex=1):
+    if command == 'install':
+        f = open('./run.bat')
+        f.write('''
+            {python} main.py run
+        '''.format(python=executable)
+        )
+        f.close()
+    if command == 'run':
+        run(command='run', argIndex=1)
+        print('FILE IS RUN')
+
+
+def read_arguments():
+    for index, arg in enumerate(argv):
+        if index > 0:
+            run(arg, index)
+
+
+def main():
+    read_arguments()
+
+
+if __name__ == '__main__':
+    main()
