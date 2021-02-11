94ad8bb08b2e721f66c7de78046f235b2abfa52a
oceaster <oceaster@gmail.com>
2021-02-11 17:33:31 +0000

    extended branch switching

--- commands/branch.py
+++ commands/branch.py
@@ -3,20 +3,24 @@ from os import system
 
 def switch(branch):
     print(
-        '\nEntering {branch_name} Branch\n\nMain :------------------------\n'.\
+        '\nEntering {branch_name} Branch\n\nParent :----------------------\n'.\
             format(branch_name=branch.\
                 replace('dev', 'Development').\
                 replace('main', 'Production')
             )
     )
+    print("Entering 'server'")
     system(
         'git checkout {branch} && git pull origin {branch} || true'.format(
             branch=branch
         )
     )
+    system('git branch --set-upstream-to=origin/{branch} {branch}'.format(
+        branch=branch
+    ))
     print('\n\nSubmodules :------------------\n')
     system(
-        'git submodule foreach --recursive "git checkout {branch} && git pull origin {branch} && echo "" || true"'.\
+        'git submodule foreach --recursive "git checkout {branch} && git pull origin {branch} && git branch --set-upstream-to=origin/{branch} {branch} && echo "" || true"'.\
         format(
             branch=branch
         )
