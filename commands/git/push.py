91eb0f3ac567c97512ff02f5f2990fc6e434a230
oceaster <oceaster@gmail.com>
2021-02-24 23:11:52 +0000

    improved push logging

--- commands/git/push.py
+++ commands/git/push.py
@@ -4,6 +4,6 @@ from os import system
 def all():
     print('\nSubmodules :------------------\n')
     system('''git submodule foreach --recursive "git push && echo ''"''')
-    print('\n\nParent :----------------------\n')
+    print('\nParent :----------------------\n')
     system('git push')
     print('')
