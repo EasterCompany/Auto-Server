a1f8c34bd14dd0f40eab4151cb37b46f959e7e36
oceaster <oceaster@gmail.com>
2021-05-08 12:28:45 +0100

    fixed server.josn pre-install dependency bug

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 from sys import path
+from os.path import exists
 from .commands.install import \
-    make_clients_config, __init_conifg_directory__, __init_logs_directory__
+    make_clients_config, __init_conifg_directory__, __init_logs_directory__,\
+    make_server_config
 from .commands.node.share import \
     __update_shared_files__
 
@@ -9,3 +11,6 @@ __init_conifg_directory__()
 __init_logs_directory__()
 __update_shared_files__()
 make_clients_config(path[0])
+
+if not exists (path[0] + '/.config/server.json'):
+    make_server_config(path[0])
